96e68d5611e4279ed0df186783ac1bec7f1fdf60
Maiko van der Veen <maikovdveen@gmail.com>
2025-03-24 17:27:53 +0100

    Add basic querying for users using URL params.

--- connect-database.js
+++ connect-database.js
@@ -1,42 +1,62 @@
-import fs from "fs";
 import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 const dbFile = "database.db";
 
-let dbInstance;
 
-export async function connectDB() { 
-  if (dbInstance) {
-    console.log("dbInstance exists.");
-    return dbInstance;
-  } else {
-    console.log("dbInstance does not exist.");
+// Returns a connection to the database. Connection is closed automatically when variable is garbage collected.
+export async function connectDB() {
+  return open({
+    filename: dbFile,
+    driver: sqlite3.Database
+  });
+}
 
-  }
+// Fill the database with tables if they're not present yet. Runs on server startup.
+export async function initDB() {
+  const db = await connectDB();
 
-  const exists = fs.existsSync(dbFile);
-  if (!exists) {
-    fs.openSync(dbFile, "w");
-  }
-
-  dbInstance = new sqlite3.Database(dbFile);
-
-  dbInstance.serialize(() => {
-    dbInstance.run(`
+  await db.exec(`
       CREATE TABLE IF NOT EXISTS Students (
       id             integer         PRIMARY KEY AUTOINCREMENT,
       first_name     VARCHAR(32)     NOT NULL,
       last_name      VARCHAR(32)     NOT NULL
       );
     `);
+}
 
-  return dbInstance;
+// export async function connectDB() { 
+//   if (dbInstance) {
+//     console.log("dbInstance exists.");
+//     return dbInstance;
+//   } else {
+//     console.log("dbInstance does not exist.");
 
-    // const addStudentsStmt = db.prepare(`
-    //   INSERT INTO Students (first_name, last_name)
-    //   VALUES
-    //   (?, ?)
-    // `);
-    // addStudentsStmt.run("Maiko", "van der Veen");
-    // addStudentsStmt.run("Bessel", "Withoos");
-  });
-};
+//   }
+
+//   const exists = fs.existsSync(dbFile);
+//   if (!exists) {
+//     fs.openSync(dbFile, "w");
+//   }
+
+//   dbInstance = new sqlite3.Database(dbFile);
+
+//   dbInstance.serialize(() => {
+//     dbInstance.run(`
+//       CREATE TABLE IF NOT EXISTS Students (
+//       id             integer         PRIMARY KEY AUTOINCREMENT,
+//       first_name     VARCHAR(32)     NOT NULL,
+//       last_name      VARCHAR(32)     NOT NULL
+//       );
+//     `);
+
+//   return dbInstance;
+
+//     // const addStudentsStmt = db.prepare(`
+//     //   INSERT INTO Students (first_name, last_name)
+//     //   VALUES
+//     //   (?, ?)
+//     // `);
+//     // addStudentsStmt.run("Maiko", "van der Veen");
+//     // addStudentsStmt.run("Bessel", "Withoos");
+//   });
+// };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { loginRouteHandler, registerRouteHandler } from "./profile-management.js";
-import { connectDB } from "./connect-database.js";
+import { connectDB, initDB } from "./connect-database.js";
 
 import path from "path";
 import { fileURLToPath } from "url";
@@ -11,18 +11,29 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 8010;
 
-const db = await connectDB();
-console.log(db);
+// Fills database with tables if they haven't been created yet.
+await initDB();
 
+// Add middleware.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("client"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/index.html"));
 });
-app.get("/users/:username", (req, res) => {
-  console.log("users: " + req.params.username);
-  res.sendFile(path.join(__dirname, "client/profiles/profile.html"));
+app.get("/users/:username", async (req, res) => {
+  const db = await connectDB();
+
+  try {
+    const user = await db.get(`SELECT * FROM Students WHERE first_name = ?`, req.params.username);
+    if (user) {
+      res.json(user);
+    } else {
+      res.send(`Couldn't find any user with name ${req.params.username}`);
+    }
+  } catch {
+    console.log(`An unknown error occurred while querying database for user with name ${req.params.username}`);
+  }
 });
 
 app.get("/users/:username/friends", (req, res) => {
@@ -33,15 +44,10 @@ app.get("/users/:username/friends", (req, res) => {
 app.post("/login", loginRouteHandler);
 app.post("/register", registerRouteHandler);
 
-app.get("/profile", (req, res) => {
-  console.log(db);
-  db.all('SELECT * FROM Students', [], (err, rows) => {
-    if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-    }
-    res.json(rows);
-});
+app.get("/profile", async (req, res) => {
+  const db = await connectDB();
+  const result = await db.all('SELECT * FROM Students');
+  res.json(result);
 });
 
 
